4aa57621b8eac23ade1c08014d8a077cd98234db
dikshitapatel <dikshitapatel1999@hotmail.com>
2020-07-25 19:07:12 +0530

    Added Login and Add_Question component

---
+++ AdminWebsite/admin-site/src/components/header.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Navbar,Nav } from 'react-bootstrap'
+  
+function Header(){
+  
+  return (
+    <div>
+                <div className="row">
+                    <div className="col-md-12">
+                      
+                            <Navbar bg="dark" variant="dark" expand="xl" sticky="top">
+                                <Navbar.Brand href="#home"></Navbar.Brand>
+                                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                                <Navbar.Collapse id="basic-navbar-nav">
+                                    <Nav className="mr-auto">
+                                    <Nav.Link href="/">Home</Nav.Link>
+                                    <Nav.Link href="/patients-list">Patients-List</Nav.Link>
+                                    <Nav.Link href="/donors-list">Donors-List</Nav.Link>
+                                    <Nav.Link href="/questionare">Feedback Questionare</Nav.Link>
+                                    <Nav.Link href="/feedback">Patients Feedback</Nav.Link>                                  
+                                    </Nav>
+                                </Navbar.Collapse>
+                            </Navbar>
+                            <br />
+                           
+                    </div>
+                </div>
+            </div>
+  );
+  }
+
+export default Header;
--- AdminWebsite/admin-site/src/index.js
+++ AdminWebsite/admin-site/src/index.js
@@ -3,11 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Login from './components/Login';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import  "bootstrap/dist/css/bootstrap.min.css";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Add_Question from './components/add_question.js';
+
+import Header from '../src/components/header';
+import Footer from '../src/components/footer';
+
+const routing = (
+  <Router>
+    <div>
+     <Header/>
+      <hr />
+      <Switch>
+        <Route exact path="/" component={App} />
+         
+      </Switch>
+      <Footer/>
+    </div>
+  </Router>
+);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  routing,
   document.getElementById('root')
 );
 
