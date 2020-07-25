af7e1a9298bca20314997de4c74fac45fde922cc
Tirth <tirthparekh32@gmail.com>
2020-07-25 21:06:29 +0530

    Updating Whatsapp Donor Chatbot

--- Donor_Chatbot/whatsapp_chatbot.py
+++ Donor_Chatbot/whatsapp_chatbot.py
@@ -3,7 +3,6 @@ import requests
 from twilio.twiml.messaging_response import MessagingResponse
 import pandas as pd
 
-#print(data.head())
 app = Flask(__name__)
 @app.route('/whatsapp_bot', methods=['POST'])
 def whatsapp_bot():
@@ -13,18 +12,10 @@ def whatsapp_bot():
 	msg = resp.message()
 	df = pd.read_csv('Jude_child_care.csv')
 	responded = False
-	#print(incoming_msg)
-	#output = []
 	
 	if incoming_msg == 'patient_id 1960':
-		#patient_id = data['PATIENT_ID']
-		#frame = {'patient_id' : patient_id}
-		#df = pd.DataFrame(frame)
-		#for i in range(len(data)):
-			#if data['PATIENT_ID'] == 1960:
 		patient_id = df.loc[df['SPACE2PATIENT_ID'] == 25810]
 		output = patient_id['SPACE_CODE'].values[0]
-		#output = output.to_string()
 		print(output)
 
 	else:
