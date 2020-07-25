49874a99396f76b1dc087baf4bd196f372ebaa80
dikshitapatel <dikshitapatel1999@hotmail.com>
2020-07-25 19:44:24 +0530

    index.js updated

--- AdminWebsite/admin-site/src/index.js
+++ AdminWebsite/admin-site/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-<<<<<<< HEAD
+
 import * as serviceWorker from './serviceWorker';
 import Login from './components/Login';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
@@ -13,24 +13,6 @@ import Add_Question from './components/add_question.js';
 import Header from '../src/components/header';
 import Footer from '../src/components/footer';
 
-const routing = (
-  <Router>
-    <div>
-     <Header/>
-      <hr />
-      <Switch>
-        <Route exact path="/" component={App} />
-         
-      </Switch>
-      <Footer/>
-    </div>
-  </Router>
-);
-=======
-import Header from '../src/components/header';
-import Footer from '../src/components/footer';
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
->>>>>>> 01cba834a4acfc3886888d43cfc2dfc85f9f70ad
 
 import  "bootstrap/dist/css/bootstrap.min.css";
 const routing = (
