4c923a48a7bb82aacf085fde5b857a96695e6b56
SugamKumar7 <rosugamkumar.is17@rvce.edu.in>
2020-07-25 20:45:38 +0530

    Routing of components

--- AdminWebsite/admin-site/src/index.js
+++ AdminWebsite/admin-site/src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-
-import Login from './components/Login';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import  "bootstrap/dist/css/bootstrap.min.css";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
