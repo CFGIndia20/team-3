d88b3d058533897679fe4b592450e1a47febde95
SugamKumar7 <rosugamkumar.is17@rvce.edu.in>
2020-07-26 08:41:08 +0530

    Table rendering for donors

--- AdminWebsite/admin-site/src/components/donors.js
+++ AdminWebsite/admin-site/src/components/donors.js
@@ -1,33 +1,5 @@
-// import React from "react";
-// import { Table } from 'semantic-ui-react'
-
-// const Donors = () => {
-//     return(
-//         <Table striped>
-//             <Table.Header>
-//                 <Table.Row>
-//                     <Table.HeaderCell>Id</Table.HeaderCell>
-//                     <Table.HeaderCell>Unit Id</Table.HeaderCell>
-//                     <Table.HeaderCell>Name</Table.HeaderCell>
-//                     <Table.HeaderCell>Phone Number</Table.HeaderCell>
-//                     <Table.HeaderCell>Email Id</Table.HeaderCell>
-//                 </Table.Row>
-//             </Table.Header>
-
-//             <Table.Body>
-//                 <Table.Row>
-//                     <Table.Cell>101</Table.Cell>
-//                     <Table.Cell>M1U1</Table.Cell>
-//                     <Table.Cell>Donor 1</Table.Cell>
-//                     <Table.Cell>8574857485</Table.Cell>
-//                     <Table.Cell>donor1@gmail.com</Table.Cell>
-//                 </Table.Row>
-//             </Table.Body>
-//         </Table>
-//     );
-// };
-
-import React, { Component } from 'react';
+import React from "react";
+import { Table } from 'semantic-ui-react'
 
 class Donors extends Component {
   // Initialize the state
@@ -37,45 +9,42 @@ class Donors extends Component {
       list: []
     }
   }
-
-  // Fetch the list on first mount
-  componentDidMount() {
-    this.getList();
-  }
-
-  // Retrieves the list of items from the Express app
-  getList = () => {
-    fetch('http://localhost:4000/doners/')
-    .then(res => res.json())
-    .then(list => this.setState({ list }))
-  }
+    // Fetch the list on first mount
+    componentDidMount() {
+      this.getList();
+    }
+    // Retrieves the list of items from the Express app
+    getList = () => {
+      fetch('http://localhost:4000/doners/')
+        .then(res => res.json())
+        .then(list => this.setState({ list }))
+    }
 
   render() {
     const { list } = this.state;
+     return(
+         <Table striped>
+             <Table.Header>
+                 <Table.Row>
+                     <Table.HeaderCell>Id</Table.HeaderCell>
+                     <Table.HeaderCell>Unit Id</Table.HeaderCell>
+                     <Table.HeaderCell>Name</Table.HeaderCell>
+                     <Table.HeaderCell>Phone Number</Table.HeaderCell>
+                     <Table.HeaderCell>Email Id</Table.HeaderCell>
+                 </Table.Row>
+             </Table.Header>
 
-    return (
-      <div className="App" style={{paddingTop:"50px"}}>
-        <h1>List of Donors</h1>
-        {/* Check to see if any items are found*/}
-        {list.length ? (
-          <div>
-            {/* Render the list of items */}
-            {list.map((item) => {
-              return(
-                <div>
-                  {item.name} | {item.phone} |   {item.email}
-                </div>
-              );
-            })}
-          </div>
-        ) : (
-          <div>
-            <h2>No List Items Found</h2>
-          </div>
-        )
-      }
-      </div>
-    );
-  }
+             <Table.Body>
+                 <Table.Row key={list.d_id}> 
+                     <Table.Cell>{list.d_id}</Table.Cell>
+                     <Table.Cell>{list.u_id}</Table.Cell>
+                     <Table.Cell>{list.name}</Table.Cell>
+                     <Table.Cell>{list.phone}</Table.Cell>
+                     <Table.Cell>{list.email}</Table.Cell>
+                 </Table.Row>
+             </Table.Body>
+         </Table>
+     );
+};
 }
 export default Donors;
